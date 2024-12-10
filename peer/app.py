865ec8fb7bc87635a7511e77eaec36211ca04828
Joel D'Autilio <joeldautilio@gmail.com>
2024-12-10 19:29:27 -0300

    Pass ports as arguments to app
    Co-authored-by: Pablo Rossi <pablorossi2001@gmail.com>

--- peer/app.py
+++ peer/app.py
@@ -1,13 +1,27 @@
 import json
+import sys
 import threading
 from flask import Flask, request
 
 from .server import Server
 
+chord_port = 50051
+http_port = 5000
+join_port = None
+
+if len(sys.argv) > 1:
+    chord_port = int(sys.argv[1])
+
+if len(sys.argv) > 2:
+    http_port = int(sys.argv[2])
+
+if len(sys.argv) > 3:
+    join_port = int(sys.argv[3])
+
 
 app = Flask(__name__)
 
-server = Server('localhost', 50051)
+server = Server('localhost', chord_port, join_port)
 t = threading.Thread(target=server.serve)
 t.start()
 
@@ -29,4 +43,4 @@ def set_user_status(user_id):
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=http_port)
